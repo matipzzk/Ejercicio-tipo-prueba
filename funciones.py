ee048efd253b7456698ef5df23cb3b5da632fc21
matias <ma.imil@duocuc.cl>
2024-06-14 12:28:17 -0400

    Se crearon funciones

---
+++ funciones.py
@@ -0,0 +1,10 @@
+def opcion1():
+    pass
+def opcion2():
+    pass
+def opcion3():
+    pass
+def opcion4():
+    print("Gracias por usar el programa , adios!")
+    time.sleep(3)
+                
