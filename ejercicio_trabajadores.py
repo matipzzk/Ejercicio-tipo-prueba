77e4b90287f2ad27b0b400b319e20fbb00f1194c
matias <ma.imil@duocuc.cl>
2024-06-14 11:24:27 -0400

    opción 1 lista sin validaciones

--- ejercicio_trabajadores.py
+++ ejercicio_trabajadores.py
@@ -1,4 +1,5 @@
 import os, time
+trabajadores=[]
 
 
 while True:
@@ -10,7 +11,16 @@ while True:
     opc = int(input("Ingrese opción"))
     os.system('cls')
     if opc == 1:
-        pass
+        print("REGISTRAR TRABAJADOR")
+        nombre,apellido= input("Ingrese su nombre y apellido: ")
+        cargo = int(input("Ingrese su cargo(1:CEO,2:DESARROLADOR,3:ANALISTA): "))
+        sueldo_bruto = int(input("Ingrese sueldo bruto: "))
+        desc_salud = int(7/100*sueldo_bruto)
+        desc_afp = int(12/100*sueldo_bruto)
+        sueldo_liquido = sueldo_bruto*0.81
+        trabajador = [nombre,apellido,cargo,sueldo_bruto,desc_afp,desc_salud,sueldo_liquido]
+        trabajadores.append(trabajador)
+        print("TRABAJADOR REGISTRADO CON ÉXITO")
     elif opc ==2:
         pass
     elif opc ==3:
