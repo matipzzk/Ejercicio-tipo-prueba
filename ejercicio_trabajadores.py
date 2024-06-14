72504185f24536caab4c4eed42d6a03247471a20
matias <ma.imil@duocuc.cl>
2024-06-14 11:05:18 -0400

    Ejercicio solo con menú

---
+++ ejercicio_trabajadores.py
@@ -0,0 +1,20 @@
+import os, time
+
+
+while True:
+    print("""MENU TRABAJADORES
+    1.  Registrar Trabajador
+    2.  Listar todos los trabajadaores
+    3.  Imprimir planilña de sueldos
+    4.  Salir""")
+    opc = int(input("Ingrese opción"))
+    os.system('cls')
+    if opc == 1:
+        pass
+    elif opc ==2:
+        pass
+    elif opc ==3:
+        pass
+    else:
+        print("Gracias por usar el programa , adios!")
+        time.sleep(3)
